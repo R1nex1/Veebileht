b27942913e0862a590c71f44c69cdb3b2264572a
R1nex1 <149042391+R1nex1@users.noreply.github.com>
2024-12-02 19:27:17 +0200

    Uuendused
    Kommenteerisin asju, css ühtlustamine ja mingi clip-path muutus, et validaatorist läbi läheks

--- reisiguuru.js
+++ reisiguuru.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
+    // Vajalikud elemendid
     const svgPaths = document.querySelectorAll('svg path');
     const countryNameDisplay = document.getElementById('riigiNimi');
     const infoLeht = document.getElementById('infoleht');
@@ -6,20 +7,24 @@ document.addEventListener('DOMContentLoaded', () => {
     const nav = document.getElementById('nav');
     let countryData = {};
 
+    // Lae riikide info JSON failist
     fetch('info.json')
         .then(response => response.json())
         .then(data => countryData = data)
         .catch(error => console.error('Error loading JSON:', error));
 
+    // Lisa sündmuste kuulajad SVG kaardi elementidele
     svgPaths.forEach(path => {
         path.addEventListener('mouseover', () => handleMouseOver(path.id));
         path.addEventListener('mouseout', handleMouseOut);
         path.addEventListener('click', () => handleClick(path.id));
     });
 
+    // Mis juhtub, kui hiir liigub riigi kohale
     function handleMouseOver(countryId) {
         const info = countryData[countryId];
         if (info) {
+            // Kuva riigi info ja video
             countryNameDisplay.textContent = info.Riiginimi;
             countryNameDisplay.style.opacity = 1;
 
@@ -28,6 +33,7 @@ document.addEventListener('DOMContentLoaded', () => {
             infoLeht.querySelector('#Valuuta').textContent = `Valuuta: ${info.Valuuta}`;
             infoLeht.querySelector('#Keel').textContent = `Keel: ${info.Keel}`;
             
+            // Käivita riigi video
             const videoElement = infoLeht.querySelector('#video');
             videoElement.querySelector('source').src = info.video;
             videoElement.load();
@@ -37,12 +43,14 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     }
 
+    // Mis juhtub, kui hiir liigub riigilt välja
     function handleMouseOut() {
         countryNameDisplay.textContent = 'Nimi';
         countryNameDisplay.style.opacity = 0;
         infoLeht.style.opacity = 0;
     }
 
+    // Mis juhtub, kui kasutaja klõpsab riigi alamlehele
     function handleClick(countryId) {
         const info = countryData[countryId];
         if (info) {
@@ -50,6 +58,7 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     }
 
+    // Näita/peida navigatsioonimenüü hiire asukoha põhjal
     document.addEventListener('mousemove', (event) => {
         menuToggleContainer.style.opacity = event.clientY < 50 ? 0 : 1;
         nav.classList.toggle('show', event.clientY < 50);
